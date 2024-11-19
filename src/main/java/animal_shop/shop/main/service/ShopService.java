c284fc4c38f513bd655d8fce37df47cc1b373226
hamster0410 <gkstjsdn0011@naver.com>
2024-11-19 19:23:54 +0900

    feat - 쇼핑몰 카테고리 조회

--- src/main/java/animal_shop/shop/main/service/ShopService.java
+++ src/main/java/animal_shop/shop/main/service/ShopService.java
@@ -6,7 +6,6 @@ import animal_shop.shop.item.repository.ItemRepository;
 import animal_shop.shop.main.dto.MainDTO;
 import animal_shop.shop.main.dto.MainDTOBestResponse;
 import animal_shop.shop.main.dto.MainDTOResponse;
-import com.sun.tools.javac.Main;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,10 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class ShopService {
