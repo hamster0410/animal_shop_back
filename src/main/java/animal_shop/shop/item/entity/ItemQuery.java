5a482617dfc8be31e80364332e05e0883ecb3835
hamster0410 <gkstjsdn0011@naver.com>
2024-11-20 15:52:08 +0900

    fix - 상품 댓글 좋아요

---
+++ src/main/java/animal_shop/shop/item/entity/ItemQuery.java
@@ -0,0 +1,24 @@
+//package animal_shop.shop.item.entity;
+//
+//import animal_shop.shop.item.dto.OptionDTO;
+//import jakarta.persistence.*;
+//
+//import java.util.List;
+//
+//@Entity
+//public class ItemQuery {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name="item_query_id")
+//    private Long id;
+//
+//    private String seller;
+//
+//    private String product;
+//
+//    private String contents;
+//
+//    private String reply;
+//
+//    private List<OptionDTO> option;
+//}
