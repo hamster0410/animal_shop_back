feb4e456c76e0ead5f41eaaa6a28fed02e14b236
hamster0410 <gkstjsdn0011@naver.com>
2024-11-25 11:55:04 +0900

    fix - item detail  진짜 수정

--- src/main/java/animal_shop/shop/item/dto/ItemDetailDTO.java
+++ src/main/java/animal_shop/shop/item/dto/ItemDetailDTO.java
@@ -14,8 +14,6 @@ public class ItemDetailDTO {
 
    private String name;
 
-   private Long optionId;
-
    private List<OptionDTO> options;
 
    private String item_detail;
@@ -42,7 +40,7 @@ public class ItemDetailDTO {
        this.name = item.getName();
        List<OptionDTO> list = new ArrayList<>();
        for (Option option : item.getOptions()) {
-           OptionDTO optionDTO = new OptionDTO(option.getName(), option.getPrice(),item.getOptions().get(0).getId());
+           OptionDTO optionDTO = new OptionDTO(option.getName(), option.getPrice(), option.getId());
            list.add(optionDTO);
        }
        this.sell_status = item.getItemSellStatus();
