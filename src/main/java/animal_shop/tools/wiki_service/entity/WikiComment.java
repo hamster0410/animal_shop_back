f29df8ffccbe496c74b957de5c13bc279e666ec4
Seok <dev.hyuk98@gmail.com>
2024-12-04 18:06:08 +0900

    feat: 백과사전 CRUD

--- src/main/java/animal_shop/tools/wiki_service/entity/WikiComment.java
+++ src/main/java/animal_shop/tools/wiki_service/entity/WikiComment.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @Builder
 public class WikiComment {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Long memberNickName;
