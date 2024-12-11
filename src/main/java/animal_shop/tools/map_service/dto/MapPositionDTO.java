77317459114e4c1b07df744c6e9085dc979829d7
hamster0410 <gkstjsdn0011@naver.com>
2024-12-11 10:15:19 +0900

    fix : place_description

--- src/main/java/animal_shop/tools/map_service/dto/MapPositionDTO.java
+++ src/main/java/animal_shop/tools/map_service/dto/MapPositionDTO.java
@@ -11,10 +11,12 @@ public class MapPositionDTO {
     private  Double longitude;
     private  Double latitude;
     private long map_id;
+    private String place_description;
     public MapPositionDTO(MapEntity mapEntity){
         this.facility_name = mapEntity.getFacilityName();
         this.latitude = mapEntity.getLatitude();
         this.longitude = mapEntity.getLongitude();
         this.map_id = mapEntity.getId();
+        this.place_description = mapEntity.getPlaceDescription();
     }
 }
