6d6b6379b7934b33eb368e772f42ec3f6a62f0ad
hamster0410 <gkstjsdn0011@naver.com>
2024-12-12 17:57:53 +0900

    fix : 댓글, 평균 평점

--- src/main/java/animal_shop/tools/map_service/dto/MapPositionDTO.java
+++ src/main/java/animal_shop/tools/map_service/dto/MapPositionDTO.java
@@ -12,11 +12,20 @@ public class MapPositionDTO {
     private  Double latitude;
     private long map_id;
     private String place_description;
+    private Long total_rating;
+    private Long comment_count;
+
     public MapPositionDTO(MapEntity mapEntity){
         this.facility_name = mapEntity.getFacilityName();
         this.latitude = mapEntity.getLatitude();
         this.longitude = mapEntity.getLongitude();
         this.map_id = mapEntity.getId();
         this.place_description = mapEntity.getPlaceDescription();
+        if(comment_count==null || comment_count == 0 ){
+            this.total_rating = 0L;
+        }else{
+            this.total_rating = mapEntity.getTotalRating() / comment_count;
+        }
+        this.comment_count = mapEntity.getCommentCount();
     }
 }
